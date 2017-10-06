731433344de7c4f89fb57c4e2d0bca4e707fcb9b
sunaliseth <sunaliseth01@gmail.com>
2017-10-06 11:08:19 +0100

    added user implementation including sign in, sign out with authentication and error messages. added layout

--- app/datamapper-setup.rb
+++ app/datamapper-setup.rb
@@ -1,6 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative './models/listing'
+require_relative './models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/fakersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
