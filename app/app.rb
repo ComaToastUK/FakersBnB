63f0e67318c2770f64a61edd1c11baf709841b54
Joe Ingram <josephcingram@hotmail.co.uk>
2017-10-06 16:33:35 +0100

    Basic front end completed

--- app/app.rb
+++ app/app.rb
@@ -40,7 +40,6 @@ class FakersBnB < Sinatra::Base
   end
 
   post '/listings/filter' do
-    # p params
     session[:filter_by_location] = params[:filter_by_location]
     session[:min_price] = params[:min_price]
     session[:max_price] = params[:max_price]
